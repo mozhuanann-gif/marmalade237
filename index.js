a51c8c19e7d35bdf80cf5d09c616c9fdd9b6588b
mozhuanann-gif <mozhuanann@126.com>
2026-01-12 21:33:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App.js';
 
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  // 使用 createElement 替代 <App /> 标签，确保 .js 文件在无编译环境下运行
+  root.render(React.createElement(React.StrictMode, null, React.createElement(App)));
 }
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
