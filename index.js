e9743adbc69b29062d3bfa53397ce51466584726
mozhuanann-gif <mozhuanann@126.com>
2026-01-12 21:57:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,5 @@ import App from './App.js';
 const rootElement = document.getElementById('root');
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
-  // 使用纯 JS 方式启动 App
   root.render(React.createElement(React.StrictMode, null, React.createElement(App)));
 }
